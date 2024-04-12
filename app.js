e409874dfb33cf1d8831a87338d7e65e693d277f
Tejaswa <mathurkiit@gmail.com>
2024-04-13 05:27:14 +0530

    CORS

--- app.js
+++ app.js
@@ -11,12 +11,12 @@ const { errorHandler } = require('./utils/errorHandlers');
 
 app.use(express.json());
 
-// app.use((req, res, next) => {
-//     res.setHeader('Access-Control-Allow-Origin', '*');
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-//     next();
-// });
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
 
 
 // Routes
